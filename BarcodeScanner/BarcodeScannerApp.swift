55f39bbb1ea4bd2278ae3ea7cb4460cdebce59d1
Hiroki Kondo <kondo.matr02@gmail.com>
2022-02-15 18:08:29 +0900

    Initial Commit

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by 近藤宏輝 on 2022/02/15.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
